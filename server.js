2b8ff2b58aa4b12d775dbf122212e6d25ef3e306
ruanjian230105 <2950731743@qq.com>
2025-05-13 11:13:48 +0800

    Create server.js
    加减乘除

---
+++ server.js
@@ -0,0 +1,14 @@
+const tape=require('tape');
+function add(num1,num2){
+    return num1+num2;
+}
+function minus(num1,num2){
+    return num1-num2;
+}
+tape("function test",asserts=>{
+    asserts.equal(add(1,2),3,"finished");
+    asserts.equal(add(3,4),7,"finished");
+    asserts.equal(minus(6,4),2,"finished");
+    asserts.equal(minus(4,2),2,"finished");
+    asserts.end();
+});
